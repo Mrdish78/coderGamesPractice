188c397e097e6fe45e72ca5a01f68852447f891d
Dish <e10htht@gmail.com>
2026-02-14 10:41:20 -0500

    updating and compiling all in one file

--- questionSeven.py
+++
@@ -1,6 +0,0 @@
-myList = [1, 2, 3, 4, 5]
-myListAverage = sum(myList) / len(myList)
-
-
-print(myListAverage)
-print(myList[::-1])
---
+++ questions.py
@@ -0,0 +1,8 @@
+def questionSeven(myList, myListAverage):
+  myList = [1, 2, 3, 4, 5]
+  myListAverage = sum(myList) / len(myList)
+
+
+  print(myListAverage)
+  print(myList[::-1])
+
