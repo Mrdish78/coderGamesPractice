73bb62f22bdfd7aa0bc8dc1f2b87023b45cb5784
Dish <e10htht@gmail.com>
2026-02-21 10:31:35 -0500

    Update questions.py

--- questions.py
+++ questions.py
@@ -48,3 +48,13 @@ def questionEleven():
           weirdListSum += v
   
   print(weirdListSum)
+
+def questionTwelve():
+  list1 = [1, 2, 3]
+  list2 = [4, 5, 6]
+  dict1 = {}
+  
+  for i in range(len(list1)):
+      dict1[list1[i]] = list2[i]
+      
+  print(dict1)
